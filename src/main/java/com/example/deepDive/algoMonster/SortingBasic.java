7ea6b2a564e236b3f303041d474e637d6480826e
rishabh <rishabhmourya72@gmail.com>
2024-09-16 23:27:52 +0530

    selection sort

--- src/main/java/com/example/deepDive/algoMonster/SortingBasic.java
+++ src/main/java/com/example/deepDive/algoMonster/SortingBasic.java
@@ -8,8 +8,9 @@ public class SortingBasic {
 
     public static void main(String[] args) {
         List<Integer> unsortedList = Arrays.asList(4, 7, 1, 3, 9, 2);
-        List<Integer> sorted = insertionSort(unsortedList);
-        System.out.println(sorted);
+        List<Integer> insertionSorted = insertionSort(unsortedList);
+        List<Integer> selectionSorted = selectionSort(unsortedList);
+        System.out.println(selectionSorted);
     }
 
     private static List<Integer> insertionSort(List<Integer> list) {
@@ -21,6 +22,21 @@ public class SortingBasic {
                 list.set(current - 1, temp);
                 current--;
             }
+        }
+        return list;
+    }
+
+    private static List<Integer> selectionSort(List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            int minIndex = i;
+            for (int j = i; j < list.size(); j++) {
+                if (list.get(j) < list.get(minIndex)) {
+                    minIndex = j;
+                }
+            }
+            int temp = list.get(i);
+            list.set(i, list.get(minIndex));
+            list.set(minIndex, temp);
         }
         return list;
     }
