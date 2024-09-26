4d376629828766a652a6dd7b78cf7a7976ab1d4a
rishabh <rishabhmourya72@gmail.com>
2024-09-26 22:41:50 +0530

    find first true in sorted array

--- src/main/java/com/example/deepDive/algoMonster/BinarySearch.java
+++ src/main/java/com/example/deepDive/algoMonster/BinarySearch.java
@@ -6,8 +6,9 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
-        System.out.println(binarySearch.vanillaBinarySearch(List.of(1, 3, 6, 8, 9, 10), 8));
-        System.out.println(binarySearch.vanillaBinarySearch(List.of(1, 3, 6, 8, 9, 10), 11));
+        //System.out.println(binarySearch.vanillaBinarySearch(List.of(1, 3, 6, 8, 9, 10), 8));
+        //System.out.println(binarySearch.vanillaBinarySearch(List.of(1, 3, 6, 8, 9, 10), 11));
+        System.out.println(binarySearch.firstTrueInSortedArray(List.of(false, false, false, false, true, true)));
     }
 
     private int vanillaBinarySearch(List<Integer> arr, int target) {
@@ -25,5 +26,21 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    private int firstTrueInSortedArray(List<Boolean> sorted) {
+        int left = 0;
+        int right = sorted.size() - 1;
+        int firstEle = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (sorted.get(mid)) {
+                firstEle = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return firstEle;
     }
 }
