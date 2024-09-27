1268906c1154d150303499ad8b15d44a08275f54
rishabh <rishabhmourya72@gmail.com>
2024-09-27 23:27:18 +0530

    firstElementNotSmallerThanTarget

--- src/main/java/com/example/deepDive/algoMonster/BinarySearch.java
+++ src/main/java/com/example/deepDive/algoMonster/BinarySearch.java
@@ -8,7 +8,8 @@ public class BinarySearch {
         BinarySearch binarySearch = new BinarySearch();
         //System.out.println(binarySearch.vanillaBinarySearch(List.of(1, 3, 6, 8, 9, 10), 8));
         //System.out.println(binarySearch.vanillaBinarySearch(List.of(1, 3, 6, 8, 9, 10), 11));
-        System.out.println(binarySearch.firstTrueInSortedArray(List.of(false, false, false, false, true, true)));
+//        System.out.println(binarySearch.firstTrueInSortedArray(List.of(false, false, false, false, true, true)));
+        System.out.println(binarySearch.firstElementNotSmallerThanTarget(List.of(1, 3, 3, 5, 8, 9), 2));
     }
 
     private int vanillaBinarySearch(List<Integer> arr, int target) {
@@ -42,5 +43,21 @@ public class BinarySearch {
             }
         }
         return firstEle;
+    }
+
+    private int firstElementNotSmallerThanTarget(List<Integer> list, int target) {
+        int left = 0;
+        int right = list.size() - 1;
+        int output = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (list.get(mid) >= target) {
+                output = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return output;
     }
 }
