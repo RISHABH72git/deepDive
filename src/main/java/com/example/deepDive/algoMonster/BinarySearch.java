5ae99d665fe7e38b9352f897c8dd311a2ffc98a9
rishabh <rishabhmourya72@gmail.com>
2024-09-28 14:53:29 +0530

    find element in sorted array

--- src/main/java/com/example/deepDive/algoMonster/BinarySearch.java
+++ src/main/java/com/example/deepDive/algoMonster/BinarySearch.java
@@ -10,6 +10,7 @@ public class BinarySearch {
         //System.out.println(binarySearch.vanillaBinarySearch(List.of(1, 3, 6, 8, 9, 10), 11));
 //        System.out.println(binarySearch.firstTrueInSortedArray(List.of(false, false, false, false, true, true)));
         System.out.println(binarySearch.firstElementNotSmallerThanTarget(List.of(1, 3, 3, 5, 8, 9), 2));
+        System.out.println(binarySearch.findElementInSortedArrayWithDuplicates(List.of(1, 3, 3, 5, 8, 9), 3));
     }
 
     private int vanillaBinarySearch(List<Integer> arr, int target) {
@@ -53,6 +54,24 @@ public class BinarySearch {
             int mid = left + (right - left) / 2;
             if (list.get(mid) >= target) {
                 output = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return output;
+    }
+
+    private int findElementInSortedArrayWithDuplicates(List<Integer> list, int target) {
+        int left = 0;
+        int right = list.size() - 1;
+        int output = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (list.get(mid) == target) {
+                output = mid;
+                right = mid - 1;
+            } else if (list.get(mid) > target) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
