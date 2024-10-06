90e0bda84c77bfd07061255d46a91940ba2456d1
rishabh <rishabhmourya72@gmail.com>
2024-10-06 14:41:21 +0530

    treeMaxDepth

--- src/main/java/com/example/deepDive/algoMonster/DepthFirstSearch.java
+++ src/main/java/com/example/deepDive/algoMonster/DepthFirstSearch.java
@@ -17,10 +17,26 @@ public class DepthFirstSearch {
         return dfs(root.right, target);
     }
 
+    public static int treeMaxDepth(Node root) {
+        if (root != null) {
+            return dfsTreeMaxDepth(root) - 1;
+        } else {
+            return 0;
+        }
+    }
+
+    private static int dfsTreeMaxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(dfsTreeMaxDepth(root.left), dfsTreeMaxDepth(root.right)) + 1;
+    }
+
     public static void main(String[] args) {
         Node left = new Node(4, new Node(3, null, null), new Node(8, null, null));
         Node right = new Node(6, null, null);
         Node node = new Node(5, left, right);
         System.out.println(dfs(node, 9));
+        System.out.println(treeMaxDepth(node));
     }
 }
