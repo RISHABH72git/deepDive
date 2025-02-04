089173b69dc8ad08098cabc3325cb7e73365e84d
rishabh <rishabhmourya72@gmail.com>
2025-02-04 22:01:55 +0530

    minimalPathSum in dp

--- src/main/java/com/example/deepDive/algoMonster/DynamicProgramming.java
+++ src/main/java/com/example/deepDive/algoMonster/DynamicProgramming.java
@@ -1,6 +1,7 @@
 package com.example.deepDive.algoMonster;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class DynamicProgramming {
 
@@ -18,7 +19,32 @@ public class DynamicProgramming {
         return dp[n - 1][m - 1];
     }
 
+    private static int minimalPathSum(List<List<Integer>> grid) {
+        int m = grid.size();
+        int n = grid.get(0).size();
+        int[][] dp = new int[m][n];
+        System.out.println(Arrays.deepToString(dp));
+        dp[0][0] = grid.get(0).get(0);
+        for (int c = 1; c < n; c++) {
+            dp[0][c] = dp[0][c - 1] + grid.get(0).get(c);
+        }
+
+        for (int r = 1; r < m; r++) {
+            dp[r][0] = dp[r - 1][0] + grid.get(r).get(0);
+        }
+
+        for (int r = 1; r < m; r++) {
+            for (int c = 1; c < n; c++) {
+                dp[r][c] = Math.min(dp[r][c - 1], dp[r - 1][c]) + grid.get(r).get(c);
+            }
+        }
+        System.out.println(Arrays.deepToString(dp));
+        return dp[m - 1][n - 1];
+    }
+
     public static void main(String[] arg) {
         System.out.println(uniquePath(5, 3));
+        List<List<Integer>> grid = List.of(List.of(1, 3, 1), List.of(1, 5, 1), List.of(4, 2, 1));
+        System.out.println(minimalPathSum(grid));
     }
 }
