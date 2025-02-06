948110690f448d24338a42b13b9658897a4a6785
rishabh <rishabhmourya72@gmail.com>
2025-02-06 21:35:28 +0530

    Triangle min path sum

--- src/main/java/com/example/deepDive/algoMonster/DynamicProgramming.java
+++ src/main/java/com/example/deepDive/algoMonster/DynamicProgramming.java
@@ -42,9 +42,26 @@ public class DynamicProgramming {
         return dp[m - 1][n - 1];
     }
 
+    private static int triangleMinPathSumRecursion(List<List<Integer>> triangle, int row, int col) {
+        if (row == triangle.size()) {
+            return 0;
+        }
+        int leftSum = triangleMinPathSumRecursion(triangle, row + 1, col);
+        int rightSum = triangleMinPathSumRecursion(triangle, row + 1, col + 1);
+        System.out.println(triangle.get(row).get(col) + "  +   Min of "+ leftSum +" " + rightSum);
+        return triangle.get(row).get(col) + Math.min(leftSum, rightSum);
+    }
+
+    private static int triangleMinPathSum(List<List<Integer>> triangle) {
+        return triangleMinPathSumRecursion(triangle, 0, 0);
+    }
+
+
     public static void main(String[] arg) {
         System.out.println(uniquePath(5, 3));
         List<List<Integer>> grid = List.of(List.of(1, 3, 1), List.of(1, 5, 1), List.of(4, 2, 1));
         System.out.println(minimalPathSum(grid));
+        List<List<Integer>> triangle = List.of(List.of(2), List.of(3, 4), List.of(6, 5, 7), List.of(4, 1, 8, 3));
+        System.out.println(triangleMinPathSum(triangle));
     }
 }
