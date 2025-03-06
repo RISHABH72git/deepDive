6d0669e0ef2eb723ebf728a0dbc569c9db8f0202
rishabh <rishabhmourya72@gmail.com>
2025-03-06 19:59:15 +0530

    stack created

--- src/main/java/com/example/deepDive/cci/StacksAndQueues.java
+++ src/main/java/com/example/deepDive/cci/StacksAndQueues.java
@@ -1,5 +1,41 @@
 package com.example.deepDive.cci;
 
+import java.util.EmptyStackException;
+
 public class StacksAndQueues {
+    public class MyStack<T> {
+        private static class StackNode<T> {
+            private T data;
+            private StackNode<T> next;
+
+            public StackNode(T data) {
+                this.data = data;
+            }
+        }
+
+        private StackNode<T> top;
+
+        public T pop() {
+            if (top == null) throw new EmptyStackException();
+            T item = this.top.data;
+            this.top = this.top.next;
+            return item;
+        }
+
+        public void push(T data) {
+            StackNode<T> t = new StackNode<T>(data);
+            t.next = top;
+            top = t;
+        }
+
+        public T peek() {
+            if (top == null) throw new EmptyStackException();
+            return top.data;
+        }
+
+        public boolean isEmpty() {
+            return top == null;
+        }
+    }
 
 }
