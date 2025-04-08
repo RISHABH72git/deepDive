88d15adeee559a2e2a0f35270dc7830cebf631bd
rishabh <rishabhmourya72@gmail.com>
2025-04-08 21:16:44 +0530

    build order

--- src/main/java/com/example/deepDive/cci/GraphCCI.java
+++ src/main/java/com/example/deepDive/cci/GraphCCI.java
@@ -65,27 +65,41 @@ public class GraphCCI {
         return level;
     }
 
-    public static void main(String[] arg) {
+    public static GraphCCI buildGraph(List<String> projects, List<List<String>> dependencies) {
         GraphCCI graphCCI = new GraphCCI();
-        graphCCI.addNode("0");
-        graphCCI.addNode("1");
-        graphCCI.addNode("2");
-        graphCCI.addNode("3");
+        for (String project : projects) {
+            graphCCI.addNode(project);
+        }
 
-        graphCCI.addEdge("0", "1");
-        graphCCI.addEdge("0", "2");
+        for (List<String> depend : dependencies) {
+            graphCCI.addEdge(depend.get(0), depend.get(1));
+        }
+        return graphCCI;
+    }
 
-        graphCCI.addEdge("1", "0");
-        graphCCI.addEdge("1", "2");
-        graphCCI.addEdge("1", "3");
-
-        graphCCI.addEdge("2", "0");
-        graphCCI.addEdge("2", "1");
-
-        graphCCI.addEdge("3", "1");
-
+    public static void main(String[] arg) {
+//        GraphCCI graphCCI = new GraphCCI();
+//        graphCCI.addNode("0");
+//        graphCCI.addNode("1");
+//        graphCCI.addNode("2");
+//        graphCCI.addNode("3");
+//
+//        graphCCI.addEdge("0", "1");
+//        graphCCI.addEdge("0", "2");
+//
+//        graphCCI.addEdge("1", "0");
+//        graphCCI.addEdge("1", "2");
+//        graphCCI.addEdge("1", "3");
+//
+//        graphCCI.addEdge("2", "0");
+//        graphCCI.addEdge("2", "1");
+//
+//        graphCCI.addEdge("3", "1");
+        List<String> projects = List.of("a", "b", "c", "d", "e", "f");
+        List<List<String>> dependencies = List.of(List.of("a", "b"), List.of("f", "b"), List.of("b", "d"), List.of("f", "a"), List.of("d", "c"));
+        GraphCCI graphCCI = GraphCCI.buildGraph(projects, dependencies);
         graphCCI.printGraph();
-        int level = GraphCCI.shortestPath(graphCCI.nodes, "0", "3");
-        System.out.println(level);
+//        int level = GraphCCI.shortestPath(graphCCI.nodes, "0", "3");
+//        System.out.println(level);
     }
 }
