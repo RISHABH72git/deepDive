da9688737e3c44a112d4fde37e4ec5deccb044e7
RISHABH72git <rishabhmourya72@gmail.com>
2025-08-05 11:04:04 +0530

    remove duplicate and reverse string

--- py_deep_dive/cracking_coding_interview/two_pointers.py
+++ py_deep_dive/cracking_coding_interview/two_pointers.py
@@ -12,5 +12,27 @@ def has_pair_with_sum(arr, target):
     return False
 
 
+def remove_duplicates_sorted(arr):
+    i = 0
+    for j in range(1, len(arr)):
+        if arr[i] != arr[j]:
+            i += 1
+            arr[i] = arr[j]
+
+    return arr[:i + 1]
+
+
+def reverse_string(s):
+    left = 0
+    right = len(s) - 1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
+    return s
+
+
 if __name__ == '__main__':
     print(has_pair_with_sum([1, 2, 4, 6, 10], 18))
+    print(remove_duplicates_sorted([1, 2, 2, 2, 4, 5]))
+    print(reverse_string(list("hello")))
